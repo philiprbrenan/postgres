3b959866176a64576263e7ebc346cfd5821b5db0
philip r brenan <philiprbrenan@gmail.com>
2021-02-11 00:00:48 +0000

    Update upload.pl

--- upload.pl
+++ upload.pl
@@ -2,4 +2,4 @@
 use Data::Table::Text qw(:all);
 use GitHub::Crud;
 
-GitHub::Crud::writeFileFromCurrentRun((output.txt), readFile(q(output.txt)));
+GitHub::Crud::writeFileFromFileFromCurrentRun q(output.txt);
