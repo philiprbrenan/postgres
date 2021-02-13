ed389a7c06a4fe94bde74168f8c064f0f4d41475
philip r brenan <philiprbrenan@gmail.com>
2021-02-13 21:40:14 +0000

    Create select.js

---
+++ select.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg')
+const client     = new Client({database:"test",user:"postgres"})
+client.connect()
+
+client.query
+ ('SELECT $1::text as message', ['Hello world!'],
+  function(err, res)
+   {console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    client.end()
+   }
+ )
