8dfbba7a2d24cae06f3c8e37ecf5e644643c6c87
philip r brenan <philiprbrenan@gmail.com>
2021-02-14 02:42:13 +0000

    Update select.js

--- select.js
+++ select.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg')
-const client     = new Client({database:"test", user:"aaaa", password:'aaaa', host: 'localhost'})
+const client     = new Client({database:"test", user:'aaaa', password:'aaaa', host:'localhost'})
 client.connect()
 
 client.query
- ('SELECT $1::text as message', ['Hello world!'],
+ ('SELECT * from test',
   function(err, res)
-   {console.log(err ? err.stack : res.rows[0].message) // Hello World!
+   {console.log(err ? err.stack : res)
     client.end()
    }
  )
+
