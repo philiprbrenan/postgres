d437112b2c4726495e1dd7962182d55ae459398d
philip r brenan <philiprbrenan@gmail.com>
2021-02-14 05:10:40 +0000

    Create select.pl

---
+++ select.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+#-------------------------------------------------------------------------------
+# Test Sql from Perl
+# Philip R Brenan at appaapps dot com, Appa Apps Ltd Inc., 2021
+#-------------------------------------------------------------------------------
+use warnings FATAL => qw(all);
+use strict;
+use Data::Dump qw(dump);
+use DBI;
+
+my $d = DBI->connect('dbi:Pg:dbname=test;host=localhost','aaaa','aaaa') or die $DBI::errstr; # Connect to database
+my $s = qq(select * from test);
+say STDERR dump($d->selectall_arrayref($s, { Slice => {} }));
