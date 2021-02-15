b5c6ca8fb6c8cd230937962e40aa97654a626ca0
philip r brenan <philiprbrenan@gmail.com>
2021-02-15 03:30:58 +0000

    Create select.java

---
+++ select.java
@@ -0,0 +1,33 @@
+package Test;
+
+import java.sql.*;
+
+public class select
+ {static final String
+     URL  = "jdbc:postgresql://localhost/test",
+     USER = "aaaa",
+     PASS = "aaaa";
+
+  public static void main(String[] args)
+   {Connection conn = null;
+    Statement  stmt = null;
+    try
+     {conn = DriverManager.getConnection(URL, USER, PASS);
+
+      stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("select * from test");
+
+      while(rs.next())
+       {String name    = rs.getString(1);
+        String address = rs.getString(2);
+        System.out.print("Name: " + name+" Address: " + address+"\n");
+       }
+      rs.close();
+      stmt.close();
+      conn.close();
+     }
+    catch(Exception e)
+     {e.printStackTrace();
+     }
+   }//end main
+ }
