f8aba18ce293be05478548668cbda32df04b1b2e
philip r brenan <philiprbrenan@gmail.com>
2021-07-31 14:50:02 +0100

    Update select.pl

--- select.pl
+++ select.pl
@@ -8,6 +8,6 @@ use strict;
 use Data::Dump qw(dump);
 use DBI;
 
-my $d = DBI->connect('dbi:Pg:dbname=test;host=localhost','aaaa','aaaa') or die $DBI::errstr; # Connect to database
+my $d = DBI->connect('dbi:Pg:dbname=test','aaaa','aaaa') or die $DBI::errstr; # Connect to database
 my $s = qq(select * from test);
 say STDERR dump($d->selectall_arrayref($s, { Slice => {} }));
